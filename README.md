d1c3387290e56fec281496426d32bf37da763069
Neos21 <neos21@gmai.com>
2016-05-27 00:24:30 +0900

    First Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.js Test
+
+Node.js とそれにまつわる JavaScript のライブラリを色々試してみるリポジトリです。
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+
+gulp.task("build-js", function () {
+  browserify({
+    // ビルド対象ファイル
+    entries: ["./src.js"]
+  })
+  .bundle()    // Browserify の実行
+  .pipe(source("main.js"))    // Vinyl に変換
+  .pipe(gulp.dest("./"));    // 出力
+});
